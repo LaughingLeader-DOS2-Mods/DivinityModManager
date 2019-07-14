4de6707be8475afcbd858a58eef1be8782c9796b
LaughingLeader <laughingleader@gmail.com>
2019-07-14 14:07:55 -0500

    Reorganized projects, added a wpf project (for draggable list items)

---
+++ DivinityModManagerGUI_WPF/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows;
+
+namespace DivinityModManager.Converters
+{
+	public class BoolToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(value is bool b)
+			{
+				if(b)
+				{
+					return Visibility.Visible;
+				}
+				else
+				{
+					return Visibility.Collapsed;
+				}
+			}
+			return Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(value is Visibility visbility)
+			{
+				if(visbility == Visibility.Visible)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
---
+++ DivinityModManagerGUI_WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DivinityModManager.Models;
+using DivinityModManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DivinityModManager.Views
+{
+	public class MainWindowDebugData : MainWindowViewModel
+	{
+		public MainWindowDebugData () : base()
+		{
+			for(var i = 0; i < 15; i++)
+			{
+				var d = new DivinityModData()
+				{
+					Name = "Test" + i,
+					Author = "LaughingLeader",
+					Version = new DivinityModVersion(370871668),
+					Description = "Test"
+				};
+				d.IsEditorMod = i%2 == 0;
+				ModOrder.Add(d);
+			}
+		}
+	}
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if(e.NewValue is MainWindowViewModel viewModel)
+			{
+				viewModel.Refresh();
+			}
+		}
+	}
+}

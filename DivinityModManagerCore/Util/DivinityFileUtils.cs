e22da1847bf8ff4302c7647ac619e99ed565ec17
LaughingLeader <laughingleader@gmail.com>
2019-11-27 03:39:19 -0600

    Added a file util class

---
+++ DivinityModManagerCore/Util/DivinityFileUtils.cs
@@ -0,0 +1,36 @@
+﻿using Alphaleonis.Win32.Filesystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivinityModManager.Util
+{
+	/// <summary>
+	/// Gets a unique file name if the file already exists.
+	/// Source: https://stackoverflow.com/a/13050041
+	/// </summary>
+	public static class DivinityFileUtils
+	{
+		public static string GetUniqueFilename(string fullPath)
+		{
+			if (!Path.IsPathRooted(fullPath))
+				fullPath = Path.GetFullPath(fullPath);
+			if (File.Exists(fullPath))
+			{
+				String filename = Path.GetFileName(fullPath);
+				String path = fullPath.Substring(0, fullPath.Length - filename.Length);
+				String filenameWOExt = Path.GetFileNameWithoutExtension(fullPath);
+				String ext = Path.GetExtension(fullPath);
+				int n = 1;
+				do
+				{
+					fullPath = Path.Combine(path, String.Format("{0} ({1}){2}", filenameWOExt, (n++), ext));
+				}
+				while (File.Exists(fullPath));
+			}
+			return fullPath;
+		}
+	}
+}

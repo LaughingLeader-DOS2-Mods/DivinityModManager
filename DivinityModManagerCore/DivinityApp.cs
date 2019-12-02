88cbce7eb2bcf322d0715ee555883797943f1ae4
LaughingLeader <laughingleader@gmail.com>
2019-12-02 17:42:46 -0600

    Added an about window

--- DivinityModManagerCore/DivinityApp.cs
+++ DivinityModManagerCore/DivinityApp.cs
@@ -9,14 +9,18 @@ namespace DivinityModManager
 	public static class DivinityApp
 	{
 		public const string DIR_DATA = "Data\\";
-		public const string URL_DONATION = @"https://ko-fi.com/laughingleader";
 		public const string URL_REPO = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager";
-		public const string URL_CHANGELOG = @"https://raw.githubusercontent.com/LaughingLeader-DOS2-Mods/DivinityModManager/master/CHANGELOG.md";
+		public const string URL_CHANGELOG = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager/blob/master/CHANGELOG.md";
+		public const string URL_CHANGELOG_RAW = @"https://raw.githubusercontent.com/LaughingLeader-DOS2-Mods/DivinityModManager/master/CHANGELOG.md";
 #if DEBUG
 		public const string URL_UPDATE = @"G:\DivinityModManager\Update.xml";
 #else
 		public const string URL_UPDATE = @"https://raw.githubusercontent.com/LaughingLeader-DOS2-Mods/DivinityModManager/master/Update.xml";
 #endif
+		public const string URL_AUTHOR = @"https://github.com/LaughingLeader";
+		public const string URL_ISSUES = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager/issues";
+		public const string URL_LICENSE = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager/blob/master/LICENSE";
+		public const string URL_DONATION = @"https://ko-fi.com/laughingleader";
 
 	}
 }

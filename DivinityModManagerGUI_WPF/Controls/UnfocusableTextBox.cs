feff1f273c8c5ba91645047463a1945f54ed14c8
LaughingLeader <laughingleader@gmail.com>
2019-12-17 11:36:18 -0600

    Added search boxes for each mod list, tweaked UnfocusableTextBox so escape reverts changed, and enter updates the binding

--- DivinityModManagerGUI_WPF/Controls/UnfocusableTextBox.cs
+++ DivinityModManagerGUI_WPF/Controls/UnfocusableTextBox.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace DivinityModManager.Controls
 {
-	public partial class UnfocusableTextBox : TextBox
+	public class UnfocusableTextBox : TextBox
 	{
+		public bool CanUndoTextOnEscape
+		{
+			get { return (bool)GetValue(CanUndoTextOnEscapeProperty); }
+			set { SetValue(CanUndoTextOnEscapeProperty, value); }
+		}
+
+		public static readonly DependencyProperty CanUndoTextOnEscapeProperty =
+		  DependencyProperty.Register("CanUndoTextOnEscape",
+		  typeof(bool), typeof(UnfocusableTextBox),
+		  new PropertyMetadata(false));
+
+		public bool UpdateBindingOnFocusLost
+		{
+			get { return (bool)GetValue(UpdateBindingOnFocusLostProperty); }
+			set { SetValue(UpdateBindingOnFocusLostProperty, value); }
+		}
+
+		public static readonly DependencyProperty UpdateBindingOnFocusLostProperty =
+		  DependencyProperty.Register("UpdateBindingOnFocusLost",
+		  typeof(bool), typeof(UnfocusableTextBox),
+		  new PropertyMetadata(false));
+
 		public UnfocusableTextBox()
 		{
-			//InitializeComponent();
+			
+		}
+
+		private string lastText = "";
+
+		protected override void OnGotKeyboardFocus(KeyboardFocusChangedEventArgs e)
+		{
+			base.OnGotKeyboardFocus(e);
+			lastText = Text;
 		}
 
 		protected override void OnKeyDown(KeyEventArgs e)
@@ -21,7 +53,21 @@ namespace DivinityModManager.Controls
 			if (e.Key == Key.Return)
 			{
 				Keyboard.ClearFocus();
-			}	
+				lastText = Text;
+				if(UpdateBindingOnFocusLost)
+				{
+					var bindingExpression = BindingOperations.GetBindingExpression(this, TextBox.TextProperty);
+					if(bindingExpression != null)
+					{
+						bindingExpression.UpdateSource();
+					}
+				}
+			}
+			else if(e.Key == Key.Escape && CanUndoTextOnEscape)
+			{
+				if (CanUndoTextOnEscape && Text != lastText) Text = lastText;
+				Keyboard.ClearFocus();
+			}
 		}
 	}
 }

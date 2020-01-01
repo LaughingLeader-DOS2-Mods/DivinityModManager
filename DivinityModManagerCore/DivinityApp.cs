d31e38387ef942a03ab4800d5b04e99c89f62947
LaughingLeader <laughingleader@gmail.com>
2020-01-01 09:36:51 -0600

    Enabled DragDirectlySelectedOnly so you can't drag/drop from empty space

--- DivinityModManagerCore/DivinityApp.cs
+++ DivinityModManagerCore/DivinityApp.cs
@@ -12,11 +12,7 @@ namespace DivinityModManager
 		public const string URL_REPO = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager";
 		public const string URL_CHANGELOG = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager/blob/master/CHANGELOG.md";
 		public const string URL_CHANGELOG_RAW = @"https://raw.githubusercontent.com/LaughingLeader-DOS2-Mods/DivinityModManager/master/CHANGELOG.md";
-#if DEBUG
-		public const string URL_UPDATE = @"G:\DivinityModManager\Update.xml";
-#else
 		public const string URL_UPDATE = @"https://raw.githubusercontent.com/LaughingLeader-DOS2-Mods/DivinityModManager/master/Update.xml";
-#endif
 		public const string URL_AUTHOR = @"https://github.com/LaughingLeader";
 		public const string URL_ISSUES = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager/issues";
 		public const string URL_LICENSE = @"https://github.com/LaughingLeader-DOS2-Mods/DivinityModManager/blob/master/LICENSE";

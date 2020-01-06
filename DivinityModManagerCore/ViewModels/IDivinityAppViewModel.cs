7de5ab7ae795031248d7c616bd5d0823b62ac693
LaughingLeader <laughingleader@gmail.com>
2020-01-05 21:12:57 -0600

    Added a view model interface so the core project can interact the main viewmodel

---
+++ DivinityModManagerCore/ViewModels/IDivinityAppViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DivinityModManager.Models;
+using DynamicData.Binding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivinityModManager.ViewModels
+{
+	public interface IDivinityAppViewModel
+	{
+		ObservableCollectionExtended<DivinityModData> ActiveMods { get; set; }
+		ObservableCollectionExtended<DivinityModData> InactiveMods { get; set; }
+		ObservableCollectionExtended<DivinityProfileData> Profiles { get; set; }
+
+		int ActiveSelected { get; }
+		int InactiveSelected { get; }
+
+		void ShowAlert(string message, int alertType = 0, int timeout = 0);
+	}
+}

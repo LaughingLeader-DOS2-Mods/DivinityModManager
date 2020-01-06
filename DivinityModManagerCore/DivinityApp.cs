559d6af0b26015520142a0b54e28d9cb0a197deb
LaughingLeader <laughingleader@gmail.com>
2020-01-05 21:11:48 -0600

    Added a class for global commands, for easier context menu command binding

--- DivinityModManagerCore/DivinityApp.cs
+++ DivinityModManagerCore/DivinityApp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DivinityModManager.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,5 +22,7 @@ namespace DivinityModManager
 		public const string XML_MOD_ORDER_MODULE = @"<node id=""Module""><attribute id=""UUID"" value=""{0}"" type=""22""/></node>";
 		public const string XML_MODULE_SHORT_DESC = @"<node id=""ModuleShortDesc""><attribute id=""Folder"" value=""{0}"" type=""30""/><attribute id=""MD5"" value=""{1}"" type=""23""/><attribute id=""Name"" value=""{2}"" type=""22""/><attribute id=""UUID"" value=""{3}"" type=""22"" /><attribute id=""Version"" value=""{4}"" type=""4""/></node>";
 		public const string XML_MOD_SETTINGS_TEMPLATE = @"<?xml version=""1.0"" encoding=""UTF-8""?><save><header version=""2""/><version major=""3"" minor=""6"" revision=""4"" build=""0""/><region id=""ModuleSettings""><node id=""root""><children><node id=""ModOrder""><children>{0}</children></node><node id=""Mods""><children>{1}</children></node></children></node></region></save>";
+
+		public static DivinityGlobalCommands GlobalCommands { get; private set; } = new DivinityGlobalCommands();
 	}
 }
---
+++ DivinityModManagerCore/Util/DivinityGlobalCommands.cs
@@ -0,0 +1,70 @@
+﻿using Alphaleonis.Win32.Filesystem;
+using DivinityModManager.Models;
+using DivinityModManager.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivinityModManager.Util
+{
+	public class DivinityGlobalCommands
+	{
+		public ReactiveCommand<string, Unit> OpenInFileExplorerCommand { get; private set; }
+		public ReactiveCommand<DivinityModData, Unit> ToggleNameDisplayCommand { get; private set; }
+
+		public void OpenInFileExplorer(string path)
+		{
+			if(File.Exists(path))
+			{
+				Process.Start("explorer.exe", $"/select, \"{path}\"");
+			}
+			else if(Directory.Exists(path))
+			{
+				Process.Start("explorer.exe", $"\"{path}\"");
+			}
+			else
+			{
+				_viewModel.ShowAlert($"Error opening '{path}': File does not exist!", -1, 10);
+			}
+		}
+
+		private IDivinityAppViewModel _viewModel;
+
+		public void SetViewModel(IDivinityAppViewModel vm)
+		{
+			_viewModel = vm;
+		}
+
+		public DivinityGlobalCommands()
+		{
+			OpenInFileExplorerCommand = ReactiveCommand.Create<string>(OpenInFileExplorer);
+			ToggleNameDisplayCommand = ReactiveCommand.Create<DivinityModData>((mod) =>
+			{
+				mod.DisplayFileForName = !mod.DisplayFileForName;
+				if (_viewModel != null)
+				{
+					if (_viewModel.ActiveSelected > 1 && _viewModel.ActiveMods.Contains(mod))
+					{
+						foreach(var m in _viewModel.ActiveMods.Where(x => x.IsSelected))
+						{
+							m.DisplayFileForName = mod.DisplayFileForName;
+						}
+					}
+					else if (_viewModel.InactiveSelected > 1 && _viewModel.InactiveMods.Contains(mod))
+					{
+						foreach (var m in _viewModel.InactiveMods.Where(x => x.IsSelected))
+						{
+							m.DisplayFileForName = mod.DisplayFileForName;
+						}
+					}
+				}
+				
+			});
+		}
+	}
+}

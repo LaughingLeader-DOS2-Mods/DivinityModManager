1e4468978b58bee5112eb20150fd9eee17e40571
LaughingLeader <laughingleader@gmail.com>
2020-01-15 20:32:30 -0600

    Added a new class for missing mod data

---
+++ DivinityModManagerCore/Models/DivinityMissingModData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivinityModManager.Models
+{
+	public struct DivinityMissingModData
+	{
+		public string Name { get; set; }
+		public int Index { get; set; }
+		public string UUID { get; set; }
+		public string Author { get; set; }
+
+		public override string ToString()
+		{
+			var str = "";
+			if(Index > 0)
+			{
+				str += $"{Index}. ";
+			}
+			str += Name;
+			if(!String.IsNullOrEmpty(Author))
+			{
+				str += " by " + Author;
+			}
+			return str;
+		}
+
+		public static DivinityMissingModData FromData(DivinityModData modData)
+		{
+			return new DivinityMissingModData
+			{
+				Name = modData.Name,
+				UUID = modData.UUID,
+				Index = modData.Index,
+				Author = modData.Author
+			};
+		}
+
+		public static DivinityMissingModData FromData(DivinityLoadOrderEntry modData, List<DivinityLoadOrderEntry> orderList)
+		{
+			return new DivinityMissingModData
+			{
+				Name = modData.Name,
+				UUID = modData.UUID,
+				Index = orderList.IndexOf(modData)
+			};
+		}
+	}
+}

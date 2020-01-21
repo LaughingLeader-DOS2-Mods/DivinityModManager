805186f6f920631686085b969bad36475289a81e
LaughingLeader <laughingleader@gmail.com>
2020-01-21 09:33:26 -0600

    Added a window wrapper for WinForms controls

---
+++ DivinityModManagerGUI_WPF/WinForms/WindowWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace DivinityModManager.WinForms
+{
+    public class WindowWrapper : System.Windows.Forms.IWin32Window
+    {
+        public WindowWrapper(IntPtr handle)
+        {
+            _hwnd = handle;
+        }
+
+        public WindowWrapper(Window window)
+        {
+            _hwnd = new WindowInteropHelper(window).Handle;
+        }
+
+        public IntPtr Handle
+        {
+            get { return _hwnd; }
+        }
+
+        private IntPtr _hwnd;
+    }
+}

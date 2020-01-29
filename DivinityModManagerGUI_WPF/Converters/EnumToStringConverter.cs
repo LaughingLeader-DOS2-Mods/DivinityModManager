bf4371a2d9d53ac3f2d28954d633f0fd6f858fa2
LaughingLeader <laughingleader@gmail.com>
2020-01-29 10:05:15 -0600

    Added some enum xaml helpers

---
+++ DivinityModManagerGUI_WPF/Converters/EnumToStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DivinityModManager.Converters
+{
+	class DivinityGameLaunchWindowActionToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (DivinityGameLaunchWindowAction)Enum.Parse(typeof(DivinityGameLaunchWindowAction), value.ToString(), true);
+        }
+    }
+}

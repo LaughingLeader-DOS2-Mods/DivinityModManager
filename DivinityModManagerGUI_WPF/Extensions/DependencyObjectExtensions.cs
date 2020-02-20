ba18c0d452679f25d0085d8f62c7f75a4f05590d
LaughingLeader <laughingleader@gmail.com>
2020-02-19 23:26:05 -0600

    Moved FindVisualChildren to an extension class

---
+++ DivinityModManagerGUI_WPF/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DivinityModManager
+{
+	public static class DependencyObjectExtensions
+	{
+		public static IEnumerable<T> FindVisualChildren<T>(this DependencyObject depObj) where T : DependencyObject
+		{
+			if (depObj != null)
+			{
+				for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+				{
+					DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+					if (child != null && child is T)
+					{
+						yield return (T)child;
+					}
+
+					foreach (T childOfChild in FindVisualChildren<T>(child))
+					{
+						yield return childOfChild;
+					}
+				}
+			}
+		}
+		public static T FindVisualParent<T>(this DependencyObject depObj) where T : DependencyObject
+		{
+			if (depObj != null)
+			{
+				//get parent item
+				DependencyObject parentObject = VisualTreeHelper.GetParent(depObj);
+
+				//we've reached the end of the tree
+				if (parentObject == null) return null;
+
+				//check if the parent matches the type we're looking for
+				T parent = parentObject as T;
+				if (parent != null)
+					return parent;
+				else
+					return FindVisualParent<T>(parentObject);
+			}
+			return null;
+		}
+	}
+}

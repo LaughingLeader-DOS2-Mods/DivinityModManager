d8bd4d139aa380b158967a25347bf110d365984d
LaughingLeader <laughingleader@gmail.com>
2020-11-03 05:54:31 -0600

    Fixed up remaining references to the previous theme folder

--- DivinityModManagerGUI_WPF/Themes/DivinityColors.cs
+++ DivinityModManagerGUI_WPF/Themes/DivinityColors.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace DivinityModManager.Resources
+namespace DivinityModManager.Themes
 {
 	public static class DivinityColors
 	{
--- DivinityModManagerGUI_WPF/Themes/ResourceAliasHelper.cs
+++ DivinityModManagerGUI_WPF/Themes/ResourceAliasHelper.cs
@@ -8,17 +8,17 @@ using System.Windows;
 using System.Windows.Markup;
 using System.Xaml;
 
-namespace DivinityModManager.Resources.Themes
+namespace DivinityModManager.Themes
 {
 	internal class ResourceAliasHelper : MarkupExtension
-    {
-        public object ResourceKey { get; set; }
+	{
+		public object ResourceKey { get; set; }
 
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            IRootObjectProvider rootObjectProvider = (IRootObjectProvider)serviceProvider.GetService(typeof(IRootObjectProvider));
-            IDictionary dictionary = rootObjectProvider?.RootObject as IDictionary;
-            return dictionary?[ResourceKey];
-        }
-    }
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			IRootObjectProvider rootObjectProvider = (IRootObjectProvider)serviceProvider.GetService(typeof(IRootObjectProvider));
+			IDictionary dictionary = rootObjectProvider?.RootObject as IDictionary;
+			return dictionary?[ResourceKey];
+		}
+	}
 }

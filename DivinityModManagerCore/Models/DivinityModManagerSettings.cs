04e75d4b4b58c5638f2591748a83b0eed33cffc6
LaughingLeader <laughingleader@gmail.com>
2020-11-05 13:49:57 -0600

    Default dark mode to true

--- DivinityModManagerCore/Models/DivinityModManagerSettings.cs
+++ DivinityModManagerCore/Models/DivinityModManagerSettings.cs
@@ -159,7 +159,7 @@ namespace DivinityModManager.Models
 			set { this.RaiseAndSetIfChanged(ref lastExtractOutputPath, value); }
 		}
 
-		private bool darkThemeEnabled = false;
+		private bool darkThemeEnabled = true;
 
 		[DataMember]
 		public bool DarkThemeEnabled

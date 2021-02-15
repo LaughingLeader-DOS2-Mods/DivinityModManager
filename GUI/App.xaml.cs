d6571523f4d7280e26d70938ba4982246e3027f3
LaughingLeader <laughingleader@gmail.com>
2021-02-14 20:58:37 -0600

    Testing setting AssemblyResolve

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using DivinityModManager.Util;
+﻿using Alphaleonis.Win32.Filesystem;
+
+using DivinityModManager.Util;
 using DivinityModManager.Views;
 using ReactiveUI;
 using System;
@@ -7,6 +9,7 @@ using System.Configuration;
 using System.Data;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -21,6 +24,9 @@ namespace DivinityModManager
 	{
 		public App()
 		{
+			// Fix for loading C++ dlls from _Lib
+			AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+			//AssemblyResolver.Hook("_Lib");
 			// POCO type warning suppression
 			Splat.Locator.CurrentMutable.Register(() => new DivinityModManager.Util.CustomPropertyResolver(), typeof(ICreatesObservableForProperty));
 			WebHelper.SetupClient();
@@ -30,6 +36,24 @@ namespace DivinityModManager
 			RxApp.DefaultExceptionHandler = new RxExceptionHandler();
 			RxApp.SuppressViewCommandBindingMessage = true;
 #endif
+		}
+
+		private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+		{
+			var assyName = new AssemblyName(args.Name);
+
+			var newPath = Path.Combine("_Lib", assyName.Name);
+			if (!newPath.EndsWith(".dll"))
+			{
+				newPath += ".dll";
+			}
+
+			if (File.Exists(newPath))
+			{
+				var assy = Assembly.LoadFile(newPath);
+				return assy;
+			}
+			return null;
 		}
 
 		private void Application_Startup(object sender, StartupEventArgs e)

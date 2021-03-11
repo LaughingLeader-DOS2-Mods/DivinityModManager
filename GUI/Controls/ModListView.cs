f6e87afdf69872b6ea9ff67ed6617a89167ea60e
LaughingLeader <laughingleader@gmail.com>
2021-03-10 19:51:05 -0600

    Fix for SHIFT selection, after moving focus with CTRL, not working correctly

--- GUI/Controls/ModListView.cs
+++ GUI/Controls/ModListView.cs
@@ -1,22 +1,44 @@
 ﻿using DivinityModManager.Util.ScreenReader;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DivinityModManager.Controls
 {
 	public class ModListView : ListView
 	{
-		public ModListView() : base() { }
+		private MethodInfo getInfoMethod;
+		private MethodInfo updateAnchorMethod;
+
+		public ModListView() : base() 
+		{
+			getInfoMethod = typeof(ItemsControl).GetMethod("ItemInfoFromContainer", BindingFlags.NonPublic | BindingFlags.Instance);
+			updateAnchorMethod = typeof(ListBox).GetMethod("UpdateAnchorAndActionItem", BindingFlags.NonPublic | BindingFlags.Instance);
+		}
 
 		protected override AutomationPeer OnCreateAutomationPeer()
 		{
 			return new ModListViewAutomationPeer(this);
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			base.OnKeyDown(e);
+
+			// Fixes CTRL + Arrow keys not updating the anchored item, which then causes Shift selection to select everything between the new and old focused items
+			switch (e.Key)
+			{
+				case Key.Up:
+				case Key.Down:
+					if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+					{
+						var info = getInfoMethod.Invoke(this, new object[] { Keyboard.FocusedElement });
+						updateAnchorMethod.Invoke(this, new object[] { info });
+					}
+					break;
+			}
 		}
 	}
 }

4d1df48d95870f04dcf415a0383d4853fe3083ef
LaughingLeader <laughingleader@gmail.com>
2021-03-26 18:51:33 -0500

    Added more extension methods to get nodes/etc from LSLib

--- DivinityModManagerCore/Extensions/ResourceExtensions.cs
+++ DivinityModManagerCore/Extensions/ResourceExtensions.cs
@@ -75,5 +75,97 @@ namespace DivinityModManager.Extensions
 
 			return null;
 		}
+
+		public static bool TryFindNode(this Resource resource, string name, out Node targetNode)
+		{
+			targetNode = FindNode(resource, name);
+			return targetNode != null;
+		}
+
+		public static bool TryFindNode(this Region region, string name, out Node targetNode)
+		{
+			targetNode = FindNode(region, name);
+			return targetNode != null;
+		}
+
+		public static Region FindRegion(this Resource resource, string name)
+		{
+			foreach (var region in resource.Regions.Values)
+			{
+				if(region.RegionName == name)
+				{
+					return region;
+				}
+			}
+
+			return null;
+		}
+
+		public static bool TryFindRegion(this Resource resource, string name, out Region targetRegion)
+		{
+			foreach (var region in resource.Regions.Values)
+			{
+				if(region.RegionName == name)
+				{
+					targetRegion = region;
+					return true;
+				}
+			}
+			targetRegion = null;
+			return false;
+		}
+
+		private static void PrintDebugNode(Node node, string indent = "", int index = -1)
+		{
+			if (index > -1)
+			{
+				DivinityApp.Log($"{indent} [{index}] Node: Name({node.Name}) Children{node.ChildCount}");
+			}
+			else
+			{
+				DivinityApp.Log($"{indent} Node: Name({node.Name}) Children{node.ChildCount}");
+			}
+			
+			DivinityApp.Log($"{indent}Attributes ({node.Attributes.Count})");
+			if(node.Attributes.Count > 0)
+			{
+				foreach (var entry in node.Attributes)
+				{
+					DivinityApp.Log($"{indent}  Attribute: Name({entry.Key}) Value({entry.Value.Value}) Type({entry.Value.Type})");
+				}
+			}
+
+			DivinityApp.Log($"{indent}Children ({node.ChildCount})");
+			if(node.ChildCount > 0)
+			{
+				foreach (var entry in node.Children)
+				{
+					DivinityApp.Log($"{indent}  Child List({entry.Key})");
+					int i = 0;
+					foreach (var node2 in entry.Value)
+					{
+						PrintDebugNode(node2, indent + " ", i);
+						i++;
+					}
+				}
+			}
+		}
+
+		public static void PrintDebug(this Resource resource)
+		{
+			foreach(var kvpRegion in resource.Regions)
+			{
+				DivinityApp.Log($"Region: Key({kvpRegion.Key}) Name({kvpRegion.Value.Name}) RegionName({kvpRegion.Value.RegionName})");
+				foreach(var nodeList in kvpRegion.Value.Children)
+				{
+					DivinityApp.Log($" Children: Key({nodeList.Key})");
+					for(var i = 0; i < nodeList.Value.Count; i++)
+					{
+						var node = nodeList.Value[i];
+						PrintDebugNode(node, "  ", i);
+					}
+				}
+			}
+		}
 	}
 }

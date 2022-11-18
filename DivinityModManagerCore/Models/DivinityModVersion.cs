1ecbc05066acec36580fc3d428f51b253616a7d5
LaughingLeader <laughingleader@gmail.com>
2022-11-17 20:25:44 -0600

    Simplify version properties with reactive properties

--- DivinityModManagerCore/Models/DivinityModVersion.cs
+++ DivinityModManagerCore/Models/DivinityModVersion.cs
@@ -1,8 +1,12 @@
 ﻿using Newtonsoft.Json;
+
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,93 +15,16 @@ namespace DivinityModManager.Models
 	[JsonObject(MemberSerialization.OptIn)]
 	public class DivinityModVersion : ReactiveObject
 	{
-		private int major = 0;
+		[Reactive] public int Major { get; set; }
+		[Reactive] public int Minor { get; set; }
+		[Reactive] public int Revision { get; set; }
+		[Reactive] public int Build { get; set; }
 
-		public int Major
-		{
-			get { return major; }
-			set
-			{
-				this.RaiseAndSetIfChanged(ref major, value);
-				UpdateVersion();
-			}
-		}
+		private readonly ObservableAsPropertyHelper<string> _version;
+		public string Version => _version.Value;
 
-		private int minor = 0;
-
-		public int Minor
-		{
-			get { return minor; }
-			set
-			{
-				this.RaiseAndSetIfChanged(ref minor, value);
-				UpdateVersion();
-			}
-		}
-
-		private int revision = 0;
-
-		public int Revision
-		{
-			get { return revision; }
-			set
-			{
-				this.RaiseAndSetIfChanged(ref revision, value);
-				UpdateVersion();
-			}
-		}
-
-		private int build = 0;
-
-		public int Build
-		{
-			get { return build; }
-			set
-			{
-				this.RaiseAndSetIfChanged(ref build, value);
-				UpdateVersion();
-			}
-		}
-
-		private string version;
-
-		[JsonProperty]
-		public string Version
-		{
-			get { return version; }
-			set
-			{
-				this.RaiseAndSetIfChanged(ref version, value);
-			}
-		}
-
-		private int versionInt = 0;
-
-		[JsonProperty]
-		public int VersionInt
-		{
-			get { return versionInt; }
-			set
-			{
-				value = Math.Max(Int32.MinValue, Math.Min(value, Int32.MaxValue));
-				if(versionInt != value)
-				{
-					ParseInt(versionInt);
-					this.RaisePropertyChanged("VersionInt");
-				}
-			}
-		}
-
-		private void UpdateVersion()
-		{
-			Version = String.Format("{0}.{1}.{2}.{3}", Major, Minor, Revision, Build);
-			var nextVersion = ToInt();
-			if(nextVersion != versionInt)
-			{
-				versionInt = ToInt();
-				this.RaisePropertyChanged("VersionInt");
-			}
-		}
+		private readonly ObservableAsPropertyHelper<int> _versionInt;
+		public int VersionInt => _versionInt.Value;
 
 		public int ToInt()
 		{
@@ -106,28 +33,25 @@ namespace DivinityModManager.Models
 
 		public override string ToString()
 		{
-			return String.Format("{0}.{1}.{2}.{3}", Major, Minor, Revision, Build);
+			return Version;
 		}
 
-		public void ParseInt(int vInt, bool update=true)
+		public void ParseInt(int vInt)
 		{
-			if(versionInt != vInt)
-			{
-				versionInt = vInt;
-				this.RaisePropertyChanged("VersionInt");
-			}
-			major = (versionInt >> 28);
-			minor = (versionInt >> 24) & 0x0F;
-			revision = (versionInt >> 16) & 0xFF;
-			build = (versionInt & 0xFFFF);
-			if (update)
-			{
-				UpdateVersion();
-			}
-			this.RaisePropertyChanged("Major");
-			this.RaisePropertyChanged("Minor");
-			this.RaisePropertyChanged("Revision");
-			this.RaisePropertyChanged("Build");
+			Major = (vInt >> 28);
+			Minor = (vInt >> 24) & 0x0F;
+			Revision = (vInt >> 16) & 0xFF;
+			Build = (vInt & 0xFFFF);
+		}
+
+		public static string StringFromIndividual(int major, int minor, int revision, int build)
+		{
+			return $"{major}.{minor}.{revision}.{build}";
+		}
+
+		public static int IntFromIndividual(int major, int minor, int revision, int build)
+		{
+			return (major << 28) + (minor << 24) + (revision << 16) + (build);
 		}
 
 		public static DivinityModVersion FromInt(int vInt)
@@ -135,14 +59,19 @@ namespace DivinityModManager.Models
 			return new DivinityModVersion(vInt);
 		}
 
-		public DivinityModVersion() { }
+		public DivinityModVersion()
+		{
+			var whenAnyNum = this.WhenAnyValue(x => x.Major, x => x.Minor, x => x.Revision, x => x.Build);
+			_version = whenAnyNum.Select(v => StringFromIndividual(v.Item1, v.Item2, v.Item3, v.Item4)).ToProperty(this, nameof(Version));
+			_versionInt = whenAnyNum.Select(v => IntFromIndividual(v.Item1, v.Item2, v.Item3, v.Item4)).ToProperty(this, nameof(VersionInt));
+		}
 
-		public DivinityModVersion(int vInt)
+		public DivinityModVersion(int vInt) : this()
 		{
 			ParseInt(vInt);
 		}
 
-		public DivinityModVersion(int headerMajor, int headerMinor, int headerRevision, int headerBuild)
+		public DivinityModVersion(int headerMajor, int headerMinor, int headerRevision, int headerBuild) : this()
 		{
 			Major = headerMajor;
 			Minor = headerMinor;
